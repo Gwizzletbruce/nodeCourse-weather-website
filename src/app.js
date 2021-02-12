31af6f189cd9f6962e00b89dbd4a88b20218f8bd
Luke Nisbet <luke78910@gmail.com>
2021-02-12 17:22:10 +0000

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ const hbs = require("hbs")
 const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
 
+const app = express()
+const port = process.env.PORT || 3000
+
 
 //Define oaths for express
 const viewsPath = path.join(__dirname, "../templates/views")
 const partialsPath = path.join(__dirname, "../templates/partials")
 
-const app = express()
+
 
 //set up handlebars engine and views location
 app.set("views", viewsPath)
@@ -108,6 +111,6 @@ app.get("*", (req, res) => {
         })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is up on port 3000")
 })
